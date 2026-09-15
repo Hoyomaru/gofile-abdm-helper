1f053bffe71246768303d3a6ddb89e399ec0ddfb
Hoyomaru <155284954+Hoyomaru@users.noreply.github.com>
2026-09-15 23:13:55 +0900

    release: bump userscript version to 1.1.0

--- gofile-abdm.user.js
+++ gofile-abdm.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         GoFile ABDM Helper
 // @namespace    https://github.com/Hoyomaru/gofile-abdm-helper
-// @version      1.0.0
+// @version      1.1.0
 // @description  Select GoFile files/folders on gofile.io and send them to AB Download Manager through a localhost helper.
 // @homepageURL  https://github.com/Hoyomaru/gofile-abdm-helper
 // @supportURL   https://github.com/Hoyomaru/gofile-abdm-helper/issues
